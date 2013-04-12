8102b934e3aa9386d2dd68acb7cb5985c68684b8
ForbesLindesay <forbes@lindesay.co.uk>
2013-04-12 21:19:40 +0100

    Use newer system for programatic runner

--- test/promises-tests.js
+++ test/promises-tests.js
@@ -1,16 +1,4 @@
-var path = require('path');
-var fs = require('fs');
+var tests = require('promises-aplus-tests');
+var adapter = require('./adapter-a');
 
-global.adapter = require('./adapter-a');
-
-describe('promises-tests', function () {
-  var testsDir = path.join(path.dirname(require.resolve('promises-aplus-tests/lib/programmaticRunner.js')), 'tests');
-  var testFileNames = fs.readdirSync(testsDir);
-
-  testFileNames.forEach(function (testFileName) {
-    if (path.extname(testFileName) === ".js") {
-      var testFilePath = path.join(testsDir, testFileName);
-      require(testFilePath);
-    }
-  });
-});
+tests.mocha(adapter);
