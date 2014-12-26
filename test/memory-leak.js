cd153d0812c56f7145dedc4fd98fab3fcce7e902
Forbes Lindesay <forbes@lindesay.co.uk>
2014-12-26 23:44:26 +0000

    Add test case to demonstrate memory-leak

---
+++ test/memory-leak.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var assert = require('assert')
+var Promise = require('../')
+
+var i = 0
+var sampleA, sampleB
+
+function next() {
+  return new Promise(function (resolve) {
+    i++
+    /*
+    if (i % 100000 === 0) {
+      global.gc()
+      console.dir(process.memoryUsage())
+    }
+    */
+    if (i === 100000 && typeof global.gc === 'function') {
+      global.gc()
+      sampleA = process.memoryUsage()
+    }
+    if (i > 100000 * 5) {
+      if (typeof global.gc === 'function') {
+        global.gc()
+        sampleB = process.memoryUsage()
+        console.dir(sampleA)
+        console.dir(sampleB)
+        assert(sampleA.rss * 1.2 > sampleB.rss, 'RSS should not grow by more than 20%')
+        assert(sampleA.heapTotal * 1.2 > sampleB.heapTotal, 'heapTotal should not grow by more than 20%')
+        assert(sampleA.heapUsed * 1.2 > sampleB.heapUsed, 'heapUsed should not grow by more than 20%')
+      }
+    } else {
+      setImmediate(resolve)
+    }
+  }).then(next)
+}
+
+if (typeof global.gc !== 'function') {
+  console.warn('You must run with --expose-gc to test for memory leak.')
+}
+next().done()
