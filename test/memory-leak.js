0b46854a16b7ca398cc2ddcb5bca3e05208dd85e
Forbes Lindesay <fobes@lindesay.co.uk>
2015-04-06 18:23:55 -0700

    Don't measure resident swap size
    It does not indicate a memory leak, and is very sensitive to
    environmental factors.

--- test/memory-leak.js
+++ test/memory-leak.js
@@ -27,7 +27,6 @@ function next() {
         console.dir(sampleA)
         console.log('Memory usage at end:');
         console.dir(sampleB)
-        assert(sampleA.rss * 1.2 > sampleB.rss, 'RSS should not grow by more than 20%')
         assert(sampleA.heapTotal * 1.2 > sampleB.heapTotal, 'heapTotal should not grow by more than 20%')
         assert(sampleA.heapUsed * 1.2 > sampleB.heapUsed, 'heapUsed should not grow by more than 20%')
       }
