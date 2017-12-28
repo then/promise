bd9c488a64e10306d804319f71a833ca8e9d04f9
Black-Hole <158blackhole@gmail.com>
2017-12-28 15:17:08 +0800

    For better code specifications
    There is no need to use `Promise.prototype['finally']`

--- src/finally.js
+++ src/finally.js
@@ -3,7 +3,7 @@
 var Promise = require('./core.js');
 
 module.exports = Promise;
-Promise.prototype['finally'] = function (f) {
+Promise.prototype.finally = function (f) {
   return this.then(function (value) {
     return Promise.resolve(f()).then(function () {
       return value;
